6b8768e23646c278428872d0fd74b9b8dee42feb
mmusgrove <mmusg24@utexas.edu>
2020-04-10 09:32:49 -0500

    Adding ARDicee skeleton project

---
+++ ARDicee/ARDicee/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Matthew Musgrove on 4/10/20.
+//  Copyright © 2020 Matthew Musgrove. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        print("World Tracking is supported: \(ARWorldTrackingConfiguration.isSupported)")
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
