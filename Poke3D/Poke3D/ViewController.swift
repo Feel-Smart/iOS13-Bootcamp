d754c071afa7d45761e8e62ef5ab788a0de2391d
mmusgrove <mmusg24@utexas.edu>
2020-04-11 18:36:17 -0500

    can now render Charmander onto pokemon card

--- Poke3D/Poke3D/ViewController.swift
+++ Poke3D/Poke3D/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -58,7 +60,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             let plane = SCNPlane(
                 width: imageAnchor.referenceImage.physicalSize.width,
-                height: imageAnchor.referenceImage.physicalSize.height)
+                height: imageAnchor.referenceImage.physicalSize.height
+            )
             
             plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
             
@@ -67,6 +70,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             planeNode.eulerAngles.x = (-.pi/2)
             
             node.addChildNode(planeNode)
+            
+            if let pokeScene = SCNScene(named: "art.scnassets/Charmander.scn"){
+                
+                if let pokeNode = pokeScene.rootNode.childNodes.first {
+                    
+                    pokeNode.eulerAngles.x = (.pi/2)
+                    planeNode.addChildNode(pokeNode)
+                    
+                }
+                
+            }
             
         }
         
