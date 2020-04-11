cdbda52db323925139d8e3553458f6e5d32bacd9
mmusgrove <mmusg24@utexas.edu>
2020-04-11 11:34:59 -0500

    adding AR Pokemon Card skeleton project

---
+++ Poke3D/Poke3D/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Poke3D
+//
+//  Created by Matthew Musgrove on 4/11/20.
+//  Copyright © 2020 Matthew Musgrove. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main){
+        
+            configuration.trackingImages = imageToTrack
+            
+            configuration.maximumNumberOfTrackedImages = 1
+            
+            print("Images Successfully Added")
+            
+        }
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+}
